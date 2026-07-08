8f4e6f8b44b174b0ddb216942b4ae48c76baeb23
jtjd <teh897@gmail.com>
2026-07-07 23:59:51 -0400

    fix: migrate to ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  ...svelte.configs['flat/recommended'],
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      'no-unused-vars': 'warn'
+    }
+  }
+];
